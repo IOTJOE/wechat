8f51fb3c7972172f1b48ff67ffc26cdbe0a8f62d
jiangzhongkle <jiangzhongke@baojinsuo.com>
2018-12-16 16:58:06 +0800

    首次提交

---
+++ src/main/java/com/baolishang/springboot/handler/ClickHandler.java
@@ -0,0 +1,54 @@
+package com.baolishang.springboot.handler;
+
+import com.baolishang.springboot.services.CoreService;
+import com.baolishang.springboot.utils.SMSHttpRequest;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 用户关注公众号Handler
+ * <p>
+ * Created by FirenzesEagle on 2016/7/27 0027.
+ * Email:liumingbo2008@gmail.com
+ */
+@Component
+public class ClickHandler extends AbstractHandler {
+
+    @Autowired
+    protected WxMpConfigStorage configStorage;
+    @Autowired
+    protected WxMpService wxMpService;
+    @Autowired
+    protected CoreService coreService;
+
+    @Value("${baojinsuo_url}")
+    private String baojinsuoUrl;
+
+    //定义servername==>目标地址
+    //点击事件转发
+    //事件点击       编号 名称  事件     转发地址
+    //t_click_event id  name eventKey url
+
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
+        logger.info("openId:"+wxMessage.getFromUser()+"===serverName=="+ wxMessage.getEventKey()+"==url=="+baojinsuoUrl+"wxuser/notify" );
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("openId", wxMessage.getFromUser());
+        map.put("serverName", wxMessage.getEventKey());
+        //发送 POST 请求
+        String sr= SMSHttpRequest.sendPost(baojinsuoUrl+"wxuser/notify",map);
+        logger.info("ResponseMessage:"+sr);
+        return null;
+    }
+};
---
+++ src/main/java/com/baolishang/springboot/repositories/MessageTemplateRepository.java
@@ -0,0 +1,21 @@
+package com.baolishang.springboot.repositories;
+
+import com.baolishang.springboot.models.MessageTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by hello on 2016/9/12.
+ */
+@Transactional
+public interface MessageTemplateRepository
+        extends JpaRepository<MessageTemplate, Long>
+{
+
+    @Query("select st from MessageTemplate st where st.name= :name ")
+    MessageTemplate getMessageTemplate(@Param("name") String name);
+//    @Query("select st from WeChatTemplateDetail st where st.WeChatTemplateID= :weChatTemplateId ")
+//    List<WeChatTemplateDetail> getWeChatTemplateDetail(@Param("weChatTemplateId") Long weChatTemplateId);
+}
